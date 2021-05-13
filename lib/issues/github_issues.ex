e84ddd06d93509c801fb58cabb0a8b23a713e158
Daniel Kuroski <daniel.kuroski@gmail.com>
2021-05-13 22:11:35 +0200

    Decode Github response

--- lib/issues/github_issues.ex
+++ lib/issues/github_issues.ex
@@ -1,5 +1,6 @@
 defmodule Issues.GithubIssues do
   @user_agent [{"User-agent", "Elixir kuroski"}]
+  @github_url Application.get_env(:issues, :github_url)
 
   def fetch(user, project) do
     issues_url(user, project)
@@ -8,14 +9,16 @@ defmodule Issues.GithubIssues do
   end
 
   def issues_url(user, project) do
-    "https://api.github.com/repos/#{user}/#{project}/issues"
+    "#{@github_url}/repos/#{user}/#{project}/issues"
   end
 
-  def handle_response({:ok, %{status_code: 200, body: body}}) do
-    {:ok, body}
+  def handle_response({:ok, %{status_code: status_code, body: body}}) do
+    {
+      status_code |> check_for_error(),
+      body |> Poison.Parser.parse!(%{})
+    }
   end
 
-  def handle_response({_, %{status_code: _, body: body}}) do
-    {:error, body}
-  end
+  defp check_for_error(200), do: :ok
+  defp check_for_error(_), do: :error
 end
