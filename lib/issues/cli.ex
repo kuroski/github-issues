b41c591920c706d2868bebff84c369620e01d1c8
Daniel Kuroski <daniel.kuroski@gmail.com>
2021-05-14 07:15:52 +0200

    Take last issues

--- lib/issues/cli.ex
+++ lib/issues/cli.ex
@@ -21,10 +21,11 @@ defmodule Issues.CLI do
     System.halt(0)
   end
 
-  def process({user, project, _count}) do
+  def process({user, project, count}) do
     Issues.GithubIssues.fetch(user, project)
     |> decode_response()
     |> sort_into_descending_order()
+    |> last(count)
   end
 
   def decode_response({:ok, body}), do: body
@@ -37,6 +38,12 @@ defmodule Issues.CLI do
   def sort_into_descending_order(list_of_issues) do
     list_of_issues
     |> Enum.sort(fn i1, i2 -> i1["created_at"] >= i2["created_at"] end)
+  end
+
+  def last(list, count) do
+    list
+    |> Enum.take(count)
+    |> Enum.reverse()
   end
 
   @doc """
