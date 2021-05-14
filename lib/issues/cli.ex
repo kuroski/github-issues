bc107472fce3ef457040a5f0189851b3a22d3ff2
Daniel Kuroski <daniel.kuroski@gmail.com>
2021-05-14 07:10:28 +0200

    Sort issues into descending order

--- lib/issues/cli.ex
+++ lib/issues/cli.ex
@@ -24,6 +24,7 @@ defmodule Issues.CLI do
   def process({user, project, _count}) do
     Issues.GithubIssues.fetch(user, project)
     |> decode_response()
+    |> sort_into_descending_order()
   end
 
   def decode_response({:ok, body}), do: body
@@ -31,6 +32,11 @@ defmodule Issues.CLI do
   def decode_response({:error, error}) do
     IO.puts("Error fetching from Github: #{error["message"]}")
     System.halt(2)
+  end
+
+  def sort_into_descending_order(list_of_issues) do
+    list_of_issues
+    |> Enum.sort(fn i1, i2 -> i1["created_at"] >= i2["created_at"] end)
   end
 
   @doc """
